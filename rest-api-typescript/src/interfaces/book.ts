39dfa8f3c686b378fd7ba3b3355879bc81a97465
Suraj Karki <suraj.karki500@protonmail.com>
2021-03-09 10:33:57 +0545

    Delete

--- rest-api-typescript/src/interfaces/book.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface IBook extends Document {
-    title: string;
-    author: string;
-}
--- rest-api-typescript/src/routes/book.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import controller from '../controllers/book';
-
-const router = express.Router();
-
-router.post('/create/book', controller.createBook);
-router.get('/get/books', controller.getAllBooks);
-
-export default router;

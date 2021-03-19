8c4b8ad4204b01fdefff8a7bc34db65def6af356
surajkarki66 <suraj.karki500@protonmail.com>
2021-03-19 09:22:14 +0545

    Renamed build to dist

--- ts-express-mongoose-restapi-boilerplate/build/helpers/errorFormatter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
-    return `${msg}`;
-};
-exports.default = errorFormatter;
-//# sourceMappingURL=errorFormatter.js.map
--- ts-express-mongoose-restapi-boilerplate/build/helpers/response.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const writeServerResponse = (res, data) => {
-    const { result, statusCode, contentType } = data;
-    res.setHeader('Content-Type', contentType);
-    return res.status(statusCode).json(result);
-};
-exports.default = writeServerResponse;
-//# sourceMappingURL=response.js.map

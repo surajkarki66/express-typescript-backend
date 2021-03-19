bbaacdcaff6b30540fca0428f6077c420785a293
surajkarki66 <suraj.karki500@protonmail.com>
2021-03-19 22:33:46 +0545

    Setup development workflow.

--- ts-express-mongodb-restapi-boilerplate/src/app.ts
+++ ts-express-mongodb-restapi-boilerplate/src/app.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
 
-app.get("/", (req, res) => res.send("Hello"));
-app.get("");
-app.get("/what", (req, res) => res.send("Fuck you"));
+app.get('/', (req, res) => res.send('Hello'));
+app.get('');
+app.get('/what', (req, res) => res.send('Fuck you'));
 app.listen(5000, () => {
-  console.log("Server is running");
+    console.log('Server is running');
 });
